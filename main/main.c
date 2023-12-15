d587c7d94fb9baec13bf34913e88231b2e2ab29d
Adam Vogt <vogt.adam@gmail.com>
2023-12-15 17:31:37 -0500

    Change "float adjust" by rotating the EC11

--- main/main.c
+++ main/main.c
@@ -21,6 +21,11 @@
 #define INA 19
 #define INB 0
 
+// EC11 rotary encoder connects to GPIO 7, 11, and 5
+#define ROT_A 7
+#define ROT_B 2
+#define BUTTON 5
+
 // 1544 RPM (60*1000 / rpm)
 #define SAT_PERIOD_MS 39
 #define FWD_SAT_PERIODS 8
@@ -59,6 +64,59 @@ uint32_t hx711_read(void)
 }
 
 
+
+
+QueueHandle_t rotation_queue;
+TaskHandle_t rot_task_hdl;
+
+void IRAM_ATTR rot_isr(void *arg) {
+        xTaskNotifyFromISR(rot_task_hdl, (uint32_t) arg, eSetValueWithOverwrite, NULL);
+}
+
+void rot_task(void *arg) {
+    uint8_t last_rot_a, last_rot_b;
+    bool a_first = true;
+    uint32_t gpio_num;
+    last_rot_a = gpio_get_level(ROT_A);
+    last_rot_b = gpio_get_level(ROT_B);
+    while (true) {
+        if (xTaskNotifyWait(0, ULONG_MAX, &gpio_num, portMAX_DELAY)) {
+            uint8_t rot_a = gpio_get_level(ROT_A);
+            uint8_t rot_b = gpio_get_level(ROT_B);
+            if (rot_a != last_rot_a && rot_b != last_rot_b) {
+                a_first = gpio_num == ROT_B;
+                last_rot_a = rot_a;
+                last_rot_b = rot_b;
+                xQueueSend(rotation_queue, (void*) &a_first, (TickType_t) 10);
+            }
+        }
+    }
+}
+
+// configures the EC11 rotary encoder gpio, glitch_filter, interrupt and task
+// leading to a rotation_queue being filled with true and false for steps in
+// each direction
+//
+// compare with https://github.com/jaytlang/him use of gpio_filter
+// which interestingly passes goto labels instead of function pointers
+void ec11_init() {
+    gpio_config_t io_conf = {
+        .intr_type = GPIO_INTR_NEGEDGE,
+        .mode = GPIO_MODE_INPUT,
+        .pin_bit_mask = (1ULL << ROT_A) | (1ULL << ROT_B),
+        .pull_down_en = 1,
+        .pull_up_en = 0,
+    };
+    gpio_config(&io_conf);
+
+    gpio_isr_handler_add(ROT_A, rot_isr, (void *) ROT_A);
+    gpio_isr_handler_add(ROT_B, rot_isr, (void *) ROT_B);
+    rotation_queue = xQueueCreate(10, sizeof(bool));
+    xTaskCreate(rot_task, "rot_task", 2048, NULL, tskIDLE_PRIORITY, &rot_task_hdl);
+}
+
+
+
 #define SET_INA(x) ramp_duty(LEDC_CHANNEL_1, x)
 #define SET_INB(x) ramp_duty(LEDC_CHANNEL_0, x)
 
@@ -85,6 +143,23 @@ struct bands {
         uint32_t dead, creep_fwd, creep_fwd_duty, saturate_duty, saturate_fwd;
 };
 
+// adjust multiplies the full-speed rotation time
+float adjust = 1.0;
+// reads the rotation_queue and increments/decrements "adjust" within bounds
+void set_adjust() {
+    while (true) {
+        bool rot;
+        int status = xQueueReceive(rotation_queue, &rot, portMAX_DELAY);
+        if (status == errQUEUE_EMPTY) continue;
+        if (rot) {
+            adjust -= 0.1;
+        } else {
+            adjust += 0.1;
+        }
+        adjust = adjust < 0.1 ? 0.1 : adjust;
+        adjust = adjust > 10.0 ? 10.0 : adjust;
+    }
+}
 
 void app_main(void) {
 
@@ -124,6 +199,8 @@ void app_main(void) {
     ledc_channel.gpio_num = INB;
     ledc_channel.channel = LEDC_CHANNEL_0;
     ledc_channel_config(&ledc_channel);
+
+    gpio_install_isr_service(ESP_INTR_FLAG_LEVEL1);
 
     // set GPIO18 low, this is ground for the INB optocoupler
     gpio_config_t io_conf = {
